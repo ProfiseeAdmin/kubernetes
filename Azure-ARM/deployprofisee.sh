903346fc7991cc5bc820bfb4d427389c838edb95
Naveen Angali <Naveen.angali@profisee.com>
2024-04-05 11:43:12 -0400

    Update deployprofisee.sh

--- Azure-ARM/deployprofisee.sh
+++ Azure-ARM/deployprofisee.sh
@@ -506,7 +506,7 @@ sed -i -e 's/$PodCount/'"$PodCount"'/g' Settings.yaml
 #pre,post init script and oidcfiledata
 preInitScriptData="Cg=="
 postInitScriptData="Cg=="
-OIDCFileData="ewp9Cg=="
+OIDCFileData="Cg=="
 sed -i -e 's/$preInitScriptData/'"$preInitScriptData"'/g' Settings.yaml
 sed -i -e 's/$postInitScriptData/'"$postInitScriptData"'/g' Settings.yaml
 sed -i -e 's/$OIDCFileData/'"$OIDCFileData"'/g' Settings.yaml
